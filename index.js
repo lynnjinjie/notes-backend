58faeb9fd8fac87b6a7b00021198aa6a333a803a
jinjie <lynnjinjie@gmail.com>
2020-09-05 23:57:33 +0800

    update: part3-c

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.static('build'))
@@ -7,52 +10,31 @@ app.use(express.json())
 
 app.use(cors())
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
-
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then((notes) => {
+    res.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter((note) => note.id !== id)
-  res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
@@ -61,17 +43,48 @@ app.post('/api/notes', (req, res) => {
       error: 'content missing',
     })
   }
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     date: new Date(),
     important: body.important || false,
+  })
+
+  note.save().then((savedNote) => {
+    res.json(savedNote)
+  })
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-  console.log(note)
-  res.json(note)
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote)
+    })
+    .catch((error) => next(error))
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  // 如果抛出异常由无效id引起
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
