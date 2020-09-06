3a2dda67fc1d441937cc85e535af1f1bf0a53db1
jinjie <lynnjinjie@gmail.com>
2020-09-06 17:29:01 +0800

    uibuild

--- models/note.js
+++ models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('useFindAndModify', false)
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
