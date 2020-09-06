8c058709bed8cfbb64ec7aec19e14a244034a08b
jinjie <lynnjinjie@gmail.com>
2020-09-07 00:27:15 +0800

    update: part3-d eslint-fix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/api/notes/:id', (req, res, next) => {
 
 app.delete('/api/notes/:id', (req, res, next) => {
   Note.findByIdAndRemove(req.params.id)
-    .then((result) => {
+    .then(() => {
       res.status(204).end()
     })
     .catch((error) => next(error))
